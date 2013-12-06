4f4698f7c5663d0f91a4d6a226e0c21a55b70cc0
Jessica Ramos <audiken_92@hotmail.com>
2013-12-06 13:08:52 +0000

    jquery

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,27 @@
-/*
-$(function() {
-  $(".cell").click(function(e) {
-    $.get(e.srcElement.id);
+$(document).ready(function() {
+  $(".cell").click(function(event) {
+  	var pathname = window.location.pathname;
+  	var ruta = pathname + event.target.id;
+    $.get(ruta, function(data) {
+    	if (data == 'illegal') {
+    		alert("No se permite el movimiento")
+    	}
+    	else if (data.length < 3) {
+    		if (data.length > 0) {
+    			$("#"+event.target.id).addClass("circle");
+    			$("#"+data).addClass("cross");
+    		}
+    		else {
+    			alert("La casilla ya ha sido escogida");
+    		}
+    	}
+    	else {
+    		setTimeout(function() {
+    			url = data;
+    			$(location).attr('href', url);
+    		}, 200);
+    	}
+    });
   });
 });
-*/
+
